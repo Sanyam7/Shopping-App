859a1516d3fb96043fb2076e77fa9842e524a026
Sanyam <chandaksanyam7@gmail.com>
2025-06-07 15:56:17 +0530

    lazy loading implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import { Routes } from "react-router-dom";
-import Navbar from "./components/Navbar"
-import { Route } from "react-router-dom";
-import Home from "./pages/Home"
-import Cart from "./pages/Cart"
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { Suspense, lazy } from "react";
+
+const Home = lazy(() => import("./pages/Home"));
+const Cart = lazy(() => import("./pages/Cart"));
 
 const App = () => {
-  return (<div>
-        <div className="fixed top-0 left-0 w-full z-50 bg-slate-900">
-          <Navbar/>
-        </div>
+  return (
+    <div>
+      <div className="fixed top-0 left-0 w-full z-50 bg-slate-900">
+        <Navbar />
+      </div>
+
+      <Suspense fallback={<div></div>}>
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/cart" element={<Cart/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
-  </div>)
+      </Suspense>
+    </div>
+  );
 };
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
-import { useState, useEffect } from "react";
-import Spinner from "../components/Spinner";
-import Product from "../components/Product";
+import { useState, useEffect, lazy, Suspense } from "react";
+
+const Spinner = lazy(() => import("../components/Spinner"));
+const Product = lazy(() => import("../components/Product"));
 
 const Home = () => {
   const API_URL = "https://fakestoreapi.com/products";
@@ -9,40 +10,38 @@ const Home = () => {
 
   async function fetchProductData() {
     setLoading(true);
-
-    try{
+    try {
       const res = await fetch(API_URL);
       const data = await res.json();
-
       setPosts(data);
-    }
-    catch(error) {
+    } catch (error) {
       console.log("Error aagya ji");
       setPosts([]);
     }
     setLoading(false);
   }
 
-  useEffect( () => {
+  useEffect(() => {
     fetchProductData();
-  },[])
+  }, []);
 
   return (
     <div>
-      {
-        loading ? <Spinner />  :
-        posts.length > 0 ? 
-        (<div className="grid mt-16 xs:gridcols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-6xl p-2 mx-auto space-y-10 space-x-5 min-h-[80vh]">
-          {
-            posts.map( (post) => (
-            <Product key = {post.id} post={post}/>
-          ) )
-          }
-        </div>) :
-        <div className="flex justify-center items-center">
-          <p>No Data Found</p>
-        </div> 
-      }
+      <Suspense fallback={<div></div>}>
+        {loading ? (
+          <Spinner />
+        ) : posts.length > 0 ? (
+          <div className="grid mt-16 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-6xl p-2 mx-auto space-y-10 space-x-5 min-h-[80vh]">
+            {posts.map((post) => (
+              <Product key={post.id} post={post} />
+            ))}
+          </div>
+        ) : (
+          <div className="flex justify-center items-center mt-16">
+            <p>No Data Found</p>
+          </div>
+        )}
+      </Suspense>
     </div>
   );
 };
